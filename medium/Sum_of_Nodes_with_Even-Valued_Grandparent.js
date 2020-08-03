0736e2e4fa6df18ab3495d8f91cc627fd6c56c64
kangjietan <kangjietan@gmail.com>
2020-08-02 23:28:04 -0700

    Easy and medium problems

---
+++ medium/Sum_of_Nodes_with_Even-Valued_Grandparent.js
@@ -0,0 +1,33 @@
+
+/*
+Given a binary tree, return the sum of values of nodes with even - valued grandparent.  (A grandparent of a node is the parent of its parent, if it exists.)
+
+If there are no nodes with an even - valued grandparent, return 0.
+
+Example 1:
+Input: root = [6, 7, 8, 2, 7, 1, 3, 9, null, 1, 4, null, null, null, 5]
+Output: 18
+Explanation: The red nodes are the nodes with even - value grandparent while the blue nodes are the even - value grandparents.
+* /
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumEvenGrandparent = function (root) {
+
+};
+
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+
+let root = treeNode(6);
