2135b90a2efc343f31f35578c497d3ce63b4f458
kangjietan <kangjietan@gmail.com>
2020-08-23 19:44:27 -0700

    2nd solution with dfs

--- medium/Subsets.js
+++ medium/Subsets.js
@@ -23,20 +23,34 @@ Output:
  * @param {number[]} nums
  * @return {number[][]}
  */
+// var subsets = function (nums) {
+//   let result = [];
+//   helper(result, nums, [], 0);
+//   return result;
+// };
+
+// function helper(result, nums, subset, idx) {
+//   if (idx === nums.length) {
+//     result.push([...subset]);
+//     return;
+//   } else {
+//     helper(result, nums, subset, idx + 1);
+//     subset.push(nums[idx]);
+//     helper(result, nums, subset, idx + 1);
+//     subset.pop();
+//   }
+// }
+
 var subsets = function (nums) {
   let result = [];
-  helper(result, nums, [], 0);
+  dfs([], 0);
+
+  function dfs(current, index) {
+    result.push(current);
+    for (let i = index; i < nums.length; i++) {
+      dfs(current.concat(nums[i]), i + 1);
+    }
+  }
+
   return result;
 };
-
-function helper(result, nums, subset, idx) {
-  if (idx === nums.length) {
-    result.push([...subset]);
-    return;
-  } else {
-    helper(result, nums, subset, idx + 1);
-    subset.push(nums[idx]);
-    helper(result, nums, subset, idx + 1);
-    subset.pop();
-  }
-}
