5f06ad40cff16c2f3060ba75b68935fa363996af
kangjietan <kangjietan@gmail.com>
2020-09-05 21:25:25 -0700

    Problem and solution

---
+++ medium/Group_Anagrams.js
@@ -0,0 +1,36 @@
+/*
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Example 2:
+Input: strs = [""]
+Output: [[""]]
+
+Example 3:
+Input: strs = ["a"]
+Output: [["a"]]
+*/
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+  let groups = {};
+  
+  for (let i = 0; i < strs.length; i++) {
+    let string = strs[i].split("").sort().join("");
+    if (groups[string] === undefined) groups[string] = [];
+    groups[string].push(strs[i]);
+  }
+
+  return Object.values(groups);
+};
+
+groupAnagrams(["eat","tea","tan","ate","nat","bat"]);
