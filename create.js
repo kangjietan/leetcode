6d51c324839387cf307184e7977b76db81e5c2e6
kangjietan <kangjietan@gmail.com>
2021-02-06 16:04:49 -0800

    Create folder and file with problem name

---
+++ create.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+let arguments = process.argv.slice(2);
+let difficulty = arguments[0];
+
+let problemName = arguments.slice(1).join('_') + '.ts';
+
+let folderName = arguments.slice(1).map((str) => str.toLowerCase()).join('-');
+let folderPath = path.join(__dirname, `./${difficulty}`);
+
+let directory = folderPath + '/' + folderName;
+
+fs.mkdir(directory, (err) => {
+  if(err) throw err;
+
+  fs.open(`${directory}/${problemName}`, "w", function (err, file) {
+    if (err) throw err;
+    if (file) console.log('Created');
+  });
+});
