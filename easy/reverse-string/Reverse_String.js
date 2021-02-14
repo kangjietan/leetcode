54b4fb4e8fc796e7294affa3822a8590d5b12d40
kangjietan <kangjietan@gmail.com>
2021-02-14 15:58:45 -0800

    Spacing.

--- easy/reverse-string/Reverse_String.js
+++ easy/reverse-string/Reverse_String.js
@@ -19,7 +19,7 @@ Output: ["h","a","n","n","a","H"]
  * @return {void} Do not return anything, modify s in-place instead.
  */
 function reverseString(s) {
-    for (let i = 0; i < s.length / 2; i++) {
-        [s[i], s[s.length - i - 1]] = [s[s.length - i - 1], s[i]];
-    }
+  for (let i = 0; i < s.length / 2; i++) {
+    [s[i], s[s.length - i - 1]] = [s[s.length - i - 1], s[i]];
+  }
 }
