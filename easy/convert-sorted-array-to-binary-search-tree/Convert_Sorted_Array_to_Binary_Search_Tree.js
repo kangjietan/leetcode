706498946ee1ef31cf50a6724e017c99e407f324
kangjietan <kangjietan@gmail.com>
2021-02-14 15:54:05 -0800

    Convert Sorted Array to Binary Search Tree

---
+++ easy/convert-sorted-array-to-binary-search-tree/Convert_Sorted_Array_to_Binary_Search_Tree.js
@@ -0,0 +1,44 @@
+"use strict";
+/**
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+Example:
+
+Given the sorted array: [-10,-3,0,5,9],
+
+One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+*/
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+  constructor(val, left, right) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+function sortedArrayToBST(nums) {
+  let head = helper(0, nums.length - 1, nums);
+  return head;
+}
+function helper(start, end, nums) {
+  if (start > end) return null;
+  let mid = Math.floor((start + end) / 2);
+  const root = new TreeNode(nums[mid]);
+  root.left = helper(start, mid - 1, nums);
+  root.right = helper(mid + 1, end, nums);
+  return root;
+}
---
+++ easy/convert-sorted-array-to-binary-search-tree/Convert_Sorted_Array_to_Binary_Search_Tree.ts
@@ -0,0 +1,50 @@
+/**
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+Example:
+
+Given the sorted array: [-10,-3,0,5,9],
+
+One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+*/
+
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+function sortedArrayToBST(nums: number[]): TreeNode | null {
+  let head = helper(0, nums.length - 1, nums);
+  return head;
+}
+
+function helper(start: number, end: number, nums: number[]): TreeNode | null {
+  if (start > end) return null;
+
+  let mid = Math.floor((start + end) / 2);
+  const root = new TreeNode(nums[mid]);
+  root.left = helper(start, mid - 1, nums);
+  root.right = helper(mid + 1, end, nums);
+  return root;
+}
