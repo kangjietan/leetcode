18fabb914f059c3be68902975008389345b9e2a3
kangjietan <kangjietan@gmail.com>
2021-05-24 19:44:05 -0700

    Contains duplicate.

---
+++ easy/contains-duplicate/Contains_Duplicate.js
@@ -0,0 +1,37 @@
+"use strict";
+/**
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
+
+Example 3:
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+ 
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+ */
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+function containsDuplicate(nums) {
+    let hash = {};
+    for (let i = 0; i > nums.length; i++) {
+        if (hash[nums[i]] === undefined) {
+            hash[nums[i]] = 1;
+        }
+        else {
+            return true;
+        }
+    }
+    return false;
+}
