22f5fb399951603ab11d7d36e4236e05391c2488
kangjietan <kangjietan@gmail.com>
2021-05-25 21:36:40 -0700

    Solution.

---
+++ easy/merge-two-sorted-lists/Merge_Two_Sorted_Lists.ts
@@ -0,0 +1,62 @@
+/**
+Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Example 1:
+Input: l1 = [1,2,4], l2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+Input: l1 = [], l2 = []
+Output: []
+
+Example 3:
+Input: l1 = [], l2 = [0]
+Output: [0]
+ 
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both l1 and l2 are sorted in non-decreasing order.
+Accepted
+ */
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+function mergeTwoLists(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {
+  if (!l1 || !l2) return l1 ? l1 : l2;
+  const tempHead = new ListNode(0);
+  let currentNode = tempHead;
+
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      currentNode.next = l1;
+      l1 = l1.next;
+    } else {
+      currentNode.next = l2;
+      l2 = l2.next;
+    }
+
+    currentNode = currentNode.next;
+  }
+
+  currentNode.next = l1 || l2;
+
+  return tempHead.next;
+}
